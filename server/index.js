446b4fbba572001201cba63e706ed4d889a71518
emmatestqaw <emma+test@qawolf.com>
2026-06-01 08:49:23 +0100

    Create index.js

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const cors = require('cors');
+const expenseRoutes = require('./routes/expenses');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use('/api/expenses', expenseRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
