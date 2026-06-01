eeaecc37a994ff65b8bab66fa4cbe328610ef356
emmatestqaw <emma+test@qawolf.com>
2026-06-01 08:49:45 +0100

    Create App.jsx

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Dashboard from './pages/Dashboard';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Expense Tracker</h1>
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
